30cc3cb1df7d941d30b7d5f19f8f78fbfcf0b3a9
mostafa hassan <mostafa813765@gmail.com>
2021-09-10 20:21:46 +0200

    add search form

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 });
+
+window.addEventListener("scroll", () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+});
+
+document.querySelector("#search-icon").addEventListener("click", () => {
+  document.querySelector("#search-form").classList.toggle("active");
+});
+
+document.querySelector("#close").addEventListener("click", () => {
+  document.querySelector("#search-form").classList.remove("active");
+});
